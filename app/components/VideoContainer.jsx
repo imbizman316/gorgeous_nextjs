7b3bd4e7d8b7903cfee3410a9315c2a5c09464ff
Mikey <pride81_@hotmail.com>
2024-06-27 00:28:14 +0900

    Adjust aspect ratio

--- app/components/VideoContainer.jsx
+++ app/components/VideoContainer.jsx
@@ -4,9 +4,9 @@ function VideoContainer() {
   return (
     <div className="flex items-center justify-center p-10">
       <iframe
-        className="w-[400px] h-[300px] md:w-[600px] md:h-[500px] lg:w-[1260px] lg:h-[1000px]"
-        width="1260"
-        height="600"
+        className="w-[700px] h-[300px] md:w-[700px] md:h-[420px] lg:w-[1360px] lg:h-[800px]"
+        width="1300"
+        height="500"
         src="https://www.youtube.com/embed/3TQd2ahq6oU?si=j0bvvOmHppe7HAsh"
         title="YouTube video player"
         frameBorder="0"
