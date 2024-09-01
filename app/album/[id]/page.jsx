a85fb9eeec366f337e6abeb03889ad1a2266c886
Mikey <pride81_@hotmail.com>
2024-09-01 16:29:05 +0900

    Modal adjustment

--- app/album/[id]/page.jsx
+++ app/album/[id]/page.jsx
@@ -34,85 +34,88 @@ function AlbumPage() {
   console.log(participants);
 
   return (
-    <div className="min-h-screen py-24 flex flex-col">
-      <div className="w-full bg-black relative">
-        <video
-          id={album.videoId}
-          autoPlay
-          loop
-          className="w-full opacity-45"
-          style={{
-            backgroundImage: `url(
+    <>
+      <div className={`${showModal ? "hidden" : "block"}`}>
+        <div className={`min-h-screen py-24 flex-col flex`}>
+          <div className={`w-full bg-black relative `}>
+            <video
+              id={album.videoId}
+              autoPlay
+              loop
+              className="w-full opacity-45"
+              style={{
+                backgroundImage: `url(
           ${album.stillImage}
         )`,
-          }}
-          muted
-          playsInline
-          data-wf-ignore="true"
-          data-object-fit="cover"
-        >
-          <source src={album.video1} data-wf-ignore="true" />
-          <source src={album.video2} data-wf-ignore="true" />
-        </video>
-        <div className="absolute top-0 w-full flex items-center h-full justify-center">
-          <Image
-            className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] lg:w-[400px] lg:h-[400px]"
-            src={album.coverImage}
-            width={300}
-            height={300}
-            alt={album.title}
-          />
-        </div>
-      </div>
+              }}
+              muted
+              playsInline
+              data-wf-ignore="true"
+              data-object-fit="cover"
+            >
+              <source src={album.video1} data-wf-ignore="true" />
+              <source src={album.video2} data-wf-ignore="true" />
+            </video>
+            <div className="absolute top-0 w-full flex items-center h-full justify-center">
+              <Image
+                className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] lg:w-[400px] lg:h-[400px]"
+                src={album.coverImage}
+                width={300}
+                height={300}
+                alt={album.title}
+              />
+            </div>
+          </div>
 
-      <div className="flex flex-col items-center justify-center sm:h-[500px] md:h-auto lg:h-auto my-10">
-        <div className="flex flex-col justify-evenly items-center w-full h-full">
-          <h1 className="text-black text-2xl sm:text-3xl lg:text-5xl py-3 font-bold">
-            {album.title}
-          </h1>
-          <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row gap-10">
-            <Image
-              src={album.coverImage}
-              // layout="fill"
-              // objectFit="contain"
-              width={380}
-              height={380} // Use the actual aspect ratio of your image
-              // objectFit="cover"
-              alt={album.title}
-              className="w-auto h-auto"
-            />
-            <Spotify link={album.musicLink} className="w-auto" />
+          <div className="flex flex-col items-center justify-center sm:h-[500px] md:h-auto lg:h-auto my-10">
+            <div className="flex flex-col justify-evenly items-center w-full h-full">
+              <h1 className="text-black text-2xl sm:text-3xl lg:text-5xl py-3 font-bold">
+                {album.title}
+              </h1>
+              <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row gap-10">
+                <Image
+                  src={album.coverImage}
+                  // layout="fill"
+                  // objectFit="contain"
+                  width={380}
+                  height={380} // Use the actual aspect ratio of your image
+                  // objectFit="cover"
+                  alt={album.title}
+                  className="w-auto h-auto"
+                />
+                <Spotify link={album.musicLink} className="w-auto" />
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full h-full flex justify-center items-center my-10">
+            <iframe
+              // width="80%"
+              // height="auto"
+              style={{ width: "400px", height: "300px" }}
+              src={album.musicVideo}
+              title="tripleS 트리플에스 : Kaede.SSS"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
+              {participants.map((member) => (
+                <Image
+                  key={member.id}
+                  src={member.photos[0]}
+                  alt={member.name}
+                  width={300}
+                  height={300}
+                  onClick={() => handleGalleryClick(member.photos[0])}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="w-full h-full flex justify-center items-center my-10">
-        <iframe
-          // width="80%"
-          // height="auto"
-          style={{ width: "400px", height: "300px" }}
-          src={album.musicVideo}
-          title="tripleS 트리플에스 : Kaede.SSS"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
-          {participants.map((member) => (
-            <Image
-              key={member.id}
-              src={member.photos[0]}
-              alt={member.name}
-              width={300}
-              height={300}
-              onClick={() => handleGalleryClick(member.photos[0])}
-            />
-          ))}
-        </div>
-      </div>
-
       {showModal && (
         <GalleryModal
           gallery={participantsPhotos}
@@ -120,7 +123,7 @@ function AlbumPage() {
           setShowModal={setShowModal}
         />
       )}
-    </div>
+    </>
   );
 }
 
